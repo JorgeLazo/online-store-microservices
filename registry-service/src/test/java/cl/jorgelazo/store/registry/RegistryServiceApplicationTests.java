9a0fa70582c7b465ee9301266cd340150803e813
JorgeLazo <jorgelazo.cl@gmail.com>
2026-09-25 11:13:47 -0300

    add registry-service

---
+++ registry-service/src/test/java/cl/jorgelazo/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.jorgelazo.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
